11eae5b31592352026335971a6e91ae454083c33
James Mullane <jamesmullane05@gmail.com>
2026-04-26 10:01:22 +1200

    TEST: extend allowed testing paths

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -268,7 +268,7 @@ export interface Research {
   id: number
   title: string
   description: string
-  'Your Research File': number | Media
+  researchLink: string
   /**
    * Used for manual sorting (lower comes first)
    */
@@ -472,7 +472,7 @@ export interface StudiesSelect<T extends boolean = true> {
 export interface ResearchSelect<T extends boolean = true> {
   title?: T
   description?: T
-  'Your Research File'?: T
+  researchLink?: T
   order?: T
   staff?: T
   categories?: T
