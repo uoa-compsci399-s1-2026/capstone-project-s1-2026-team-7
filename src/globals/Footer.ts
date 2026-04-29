5ef9579fa0ef0750ac153422552b27f8bcd28b30
Carl misquitta <131878928+CarlMisquitta@users.noreply.github.com>
2026-04-30 03:56:05 +1200

    FEAT: creating footer global template

---
+++ src/globals/Footer.ts
@@ -0,0 +1,45 @@
+/* import type { GlobalConfig } from 'payload'
+
+export const Footer: GlobalConfig = {
+  slug: 'footer',
+  label: 'Footer',
+  fields: [
+    {
+      name: 'uoaLogo',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'hnuLogo',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'navbarLinks',
+      type: 'array',
+      label: 'Navigation Links',
+      labels: {
+        singular: 'Navigation Link',
+        plural: 'Navigation Links',
+      },
+      fields: [
+        {
+          name: 'navTitle',
+          type: 'text',
+          label: 'Navigation Title',
+          required: true,
+          localized: true,
+        },
+        {
+          name: 'navURL',
+          type: 'text',
+          label: 'Navigation URL',
+          required: true,
+        },
+      ],
+    },
+  ],
+}
+*/
