4268043d1787d7643b21883fe0c64ebc3b792c1f
James Mullane <jamesmullane05@gmail.com>
2026-05-14 02:59:05 +1200

    FIX: solving commit issues

--- src/app/(frontend)/[lang]/ourteam/page.tsx
+++ src/app/(frontend)/[lang]/ourteam/page.tsx
@@ -1,16 +1,8 @@
-import React from 'react'
-import ProfileCard from './Profile'
 import TeamSection from './TeamSection'
 import { getOurTeamPage } from '@/queries/ourteampage'
-import { OurTeamPageDTO, StaffDTO } from '@/validation/our-team'
-import { select } from 'payload/shared'
+import { OurTeamPageDTO } from '@/validation/our-team'
 
 export default async function OurTeam() {
-  {
-    /* Import from backend*/
-  }
-
   const pageData: OurTeamPageDTO = await getOurTeamPage('en')
-
   return <TeamSection teamSection={pageData} />
 }
